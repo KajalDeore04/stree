804d391c08296c9bd13019cb053307ef7ea18639
KajalDeore04 <kajaldeore04@gmail.com>
2025-03-19 22:31:15 +0530

    cors changed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin:  ["http://localhost:5173","https://stree-safety.vercel.app","https://stree.onrender.com"],
+    origin:  ["http://localhost:5173","https://stree-safety.vercel.app","https://stree-safety.onrender.com"],
     
     methods: "GET, POST, PUT, DELETE, PATCH",
     credentials: true,

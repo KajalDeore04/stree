e8149ae6a7323783f7f3cc70dff572360e64e6ac
KajalDeore04 <kajaldeore04@gmail.com>
2025-03-20 10:07:48 +0530

    .

--- client/src/components/layouts/admin-layouts.jsx
+++ client/src/components/layouts/admin-layouts.jsx
@@ -57,7 +57,7 @@ function AdminLayout() {
           // </div>
           <AdminDashboard/>
         ) : (
-          <div className="h-full bg-black mx-auto max-w-[1000px]">
+          <div className="h-full bg-black mx-auto max-w-[1300px]">
           <Outlet />
           </div>
         )}
